04c101d8dcf722109e9195ed840fe64f56fe91bb
GheistLycis <bruno.nitro48@gmail.com>
2022-03-25 14:48:17 -0300

    added cities button

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let buttonEnter = new ButtonEnter("div-messages", "button-enter");
 let buttonTimesTable = new ButtonTimesTable("div-messages", "button-timestable");
 let buttonActors = new ButtonActors("div-boxes", "button-actors");
 let buttonCounter = new ButtonCounter("div-messages", "button-counter");
+let buttonCities = new ButtonCities("div-messages", "button-cities");
---
+++ script/buttonCitiesController.js
@@ -0,0 +1,56 @@
+class ButtonCities{
+    constructor(divId, buttonId){
+        this.divMessages = document.getElementById(divId);
+        this.buttonCities = document.getElementById(buttonId);
+        this.cities = {
+            sp: ['Jundiaí', 'Campinas', 'Limeira', 'Atibaia'],
+            rj: ['Teresópolis', 'Resende', 'Maricá', 'Macaé'],
+        };
+        this.init();
+    }
+
+    init(){
+        this.buttonCities.addEventListener("click", ()=>{
+            //CREATING SELECT PANEL
+            let div = document.createElement("div");
+            div.setAttribute("id", "div-cities");
+            div.innerHTML = `
+                <label> State
+                    <select id="states">
+                        <option value="SP">São Paulo</option>
+                        <option value="RJ">Rio de Janeiro</option>
+                    </select>
+                </label>
+                <div id="cities">
+                </div>
+                <button id="confirm">Confirm</button>
+            `;
+            this.divMessages.appendChild(div);
+
+            //CREATING CITIES PANEL
+
+            //DISPLAYING CITIES PANEL
+            div.querySelector("#states").addEventListener("change", ()=>{
+                let option = this.stateOption();
+                if(option == "SP"){
+                    //GET INTO DIV CITIES, SHOW SP PANEL AND HIDE RJ PANEL;
+                }
+                else if(option == "RJ"){
+                    //GET INTO DIV CITIES, SHOW RJ PANEL AND HIDE SP PANEL;
+                }
+            });
+        })
+    }
+
+    stateOption(){
+        let select = document.querySelector("#states");
+        let selected;
+        for(let i = 0; i < select.options.length; i++){
+            if(select.options[i].selected){
+                selected = select.options[i].value;
+                break;
+            };
+        }
+        return selected;
+    }
+}
